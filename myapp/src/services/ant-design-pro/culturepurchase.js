6996953689b20f755a6e25797b0067668a78d124
shengyi <ysyshine13@gmail.com>
2022-05-06 22:33:09 +0800

    add culturepurchase page

--- myapp/src/services/ant-design-pro/culturepurchase.js
+++ myapp/src/services/ant-design-pro/culturepurchase.js
@@ -6,3 +6,36 @@ export async function searchCulturePurchase(options) {
     ...(options || {}),
   });
 }
+  
+export async function updateCulturepurchase(body, options) {
+  return request('/api/culturepurchase/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+  
+  export async function addCulturepurchase(body, options) {
+    return request('/api/culturepurchase/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: body,
+      ...(options || {}),
+    });
+  }
+  
+  export async function deleteCulturepurchase(body, options) {
+    return request('/api/culturepurchase/delete', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: body,
+      ...(options || {}),
+    });
+  }
